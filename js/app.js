5346bef9d34d4959a28960a246cbd47ca6d8e933
AhmedkamalR <abokamal01012@gmail.com>
2021-12-01 19:52:57 +0200

    fix navbar issue and section highlighted

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@
  * JS Standard: ESlint
  *
  */
-const currentSection = document.querySelectorAll('section');
+const currentSections = document.querySelectorAll('section');
 const Ul = document.querySelector('ul');
 const documentFragment = document.createDocumentFragment();
 const goTop = document.getElementById('topScroll');
@@ -27,52 +27,55 @@ const goTop = document.getElementById('topScroll');
  *
  */
 
-//bulid the navbar
-currentSection.forEach((section) => {
-  //Extract IdAttribute value from the section and store it in variable
+//build the navbar
+currentSections.forEach((section) => {
+  //Extract id value from the section and store it in variable
+  const navData = section.getAttribute('data-nav');
+
   const id = section.getAttribute('id');
-  const list = document.createElement('li');
-  const link = document.createElement('a');
+  const newList = document.createElement('li');
+  const links = document.createElement('a');
   // customize navbar
-  link.classList.add('menu__link');
+  links.classList.add('menu__link');
   // get ref
-  link.setAttribute('href', id);
+  links.setAttribute('href', id);
   // Scroll to target section
-  link.addEventListener('click', (e) => {
-    e.preventDefault(); // to avoid reload the page
+  links.addEventListener('click', (e) => {
+    e.preventDefault();
     section.scrollIntoView({ behavior: 'smooth' });
   });
   // add currentSection name
-  const text = document.createTextNode(section.getAttribute('data-nav'));
-  link.appendChild(text);
-  list.appendChild(link);
-  documentFragment.appendChild(list);
+  const text = document.createTextNode(navData);
+  links.appendChild(text);
+  newList.appendChild(links);
+  documentFragment.appendChild(newList);
 });
 //append in documentFragment to make performance improvements
 Ul.appendChild(documentFragment);
 
 window.addEventListener('scroll', () => {
   //  For Chek What Is Section On Screen Now and remove the active class
-  const activesec = document.getElementsByClassName('your-active-class')[0];
+  const activeSection = document.getElementsByClassName('your-active-class')[0];
 
-  activesec !== undefined
-    ? activesec.classList.remove('your-active-class')
+  activeSection !== undefined
+    ? activeSection.classList.remove('your-active-class')
     : null;
   const ActiveNav = document.getElementsByClassName('navactive')[0];
   ActiveNav !== undefined ? ActiveNav.classList.remove('navactive') : null;
 
   // Set currentSection as active
-  currentSection.forEach((section) => {
+  currentSections.forEach((section) => {
     const react = section.getBoundingClientRect();
-    if (react.Top >= -50 && react.Top < 380) {
+    if (react.top >= -50 && react.top < 380) {
       section.classList.add('your-active-class');
       const activeList = document.querySelectorAll(`a[href='${section.id}']`)[0]
         .parentElement;
+
       activeList.classList.add('navactive');
       // Go To Top section
       section.id == 'section1'
-        ? (goTOP.style.display = 'none')
-        : (goTOP.style.display = 'block');
+        ? (goTop.style.display = 'none')
+        : (goTop.style.display = 'block');
     }
   });
 });
